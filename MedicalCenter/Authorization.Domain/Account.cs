0b9f455db781876a52db0d65d225bd657360fe2d
P1zhama <vlad-serdjukov@bk.ru>
2026-07-05 13:22:09 +0300

    MedicalCenter/Authorization.Domain Add Acount

---
+++ MedicalCenter/Authorization.Domain/Account.cs
@@ -0,0 +1,27 @@
+﻿using Authorization.Domain.Enums;
+
+
+namespace Authorization.Domain;
+
+
+public class Account
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string? PhoneNumber { get; set; }
+    public bool IsEmailVerified { get; set; } = false;
+    public Guid? PhotoId { get; set; }
+
+    public UserRole Role { get; set; } = UserRole.None;
+
+
+    public string CreatedBy { get; set; } = "System";
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public string UpdatedBy { get; set; } = "System";
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
+}
---
+++ MedicalCenter/Authorization.Domain/Enums/UserRole.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Authorization.Domain.Enums;
+
+public enum UserRole
+{
+    None = 1,
+
+    Patient = 2,
+    Doctor = 4,
+    Receptionist = 8
+}
+

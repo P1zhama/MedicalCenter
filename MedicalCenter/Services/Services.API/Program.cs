3169b367cae5179e0c0183cd8cddfbe3cec482b3
P1zhama <vlad-serdjukov@bk.ru>
2026-08-06 11:32:08 +0300

    Services.Api Update

--- MedicalCenter/Services/Services.API/Program.cs
+++ MedicalCenter/Services/Services.API/Program.cs
@@ -1,12 +1,73 @@
-using Services.API.Services;
+using Common.Api.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+using Services.Api.Interceptors;
+using Services.Api.Services;
+using Services.Application;
+using Services.Infrastructure;
+using Services.Infrastructure.Persistence;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console(new CompactJsonFormatter())
+    .WriteTo.File(new CompactJsonFormatter(), "Logs/bootstrap-log-.json", rollingInterval: RollingInterval.Day)
+    .CreateBootstrapLogger();
 
-builder.Services.AddGrpc();
+try
+{
+    Log.Information("Starting Services Microservice...");
+    var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services));
 
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+    AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 
-app.Run();
+    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddApplication();
+
+    builder.Services.AddGrpc(options =>
+    {
+        options.Interceptors.Add<CorrelationIdInterceptor>();
+        options.Interceptors.Add<UserContextInterceptor>();
+        options.Interceptors.Add<GrpcExceptionInterceptor>();
+    });
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    app.MapGrpcService<ServicesGrpcService>();
+    app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+    using (var scope = app.Services.CreateScope())
+    {
+        try
+        {
+            var context = scope.ServiceProvider.GetRequiredService<ServicesDbContext>();
+
+            if (context.Database.IsSqlServer())
+            {
+                context.Database.Migrate();
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "An error occurred while performing a database migration.");
+        }
+    }
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Services microservice terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}

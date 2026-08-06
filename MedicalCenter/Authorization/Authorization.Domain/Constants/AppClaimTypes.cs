5f295a13b6fa73a3690222e88ab03d3c574efc10
P1zhama <vlad-serdjukov@bk.ru>
2026-08-06 10:26:54 +0300

    Authorization.Domain Delete permission

--- MedicalCenter/Authorization/Authorization.Domain/Constants/AppClaimTypes.cs
+++ MedicalCenter/Authorization/Authorization.Domain/Constants/AppClaimTypes.cs
@@ -3,8 +3,4 @@ namespace Authorization.Domain.Constants;
 public static class AppClaimTypes
 {
     public const string Role = "role";
-
-    public const string Permission = "permission";
-
-    public const string Department = "department";
 }
--- MedicalCenter/Authorization/Authorization.Domain/Constants/Permissions.cs
+++
@@ -1,8 +0,0 @@
-namespace Authorization.Domain.Constants;
-
-public static class Permissions
-{
-    public const string CreateWorkerAccount = "accounts:create-worker";
-
-    public const string ManageAccountPermissions = "accounts:manage-permissions";
-}

8e90ef82a02fef90c3a732e26c4221e78c995d51
P1zhama <vlad-serdjukov@bk.ru>
2026-08-13 19:27:41 +0300

    Appointments Add Empty Project

--- MedicalCenter/Appointments/Appointments.API/Program.cs
+++ MedicalCenter/Appointments/Appointments.API/Program.cs
@@ -1,4 +1,5 @@
-using Appointments.API.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +7,6 @@ builder.Services.AddGrpc();
 
 var app = builder.Build();
 
-app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();

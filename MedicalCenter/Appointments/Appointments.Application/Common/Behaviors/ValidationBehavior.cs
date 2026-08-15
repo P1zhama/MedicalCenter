adfffd677bafb2c5e467ebdf6af086bb9c029e78
P1zhama <vlad-serdjukov@bk.ru>
2026-08-15 23:22:30 +0300

    Appointments.Application Add ValidationBehavior

---
+++ MedicalCenter/Appointments/Appointments.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,44 @@
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace Appointments.Application.Common.Behaviors;
+
+public sealed class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : IErrorOr
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+            return await next();
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationResults = await Task.WhenAll(
+            _validators.Select(validator => validator.ValidateAsync(context, cancellationToken)));
+
+        var failures = validationResults
+            .SelectMany(result => result.Errors)
+            .Where(failure => failure is not null)
+            .ToList();
+
+        if (failures.Count == 0)
+            return await next();
+
+        var errors = failures.ConvertAll(
+            failure => Error.Validation(failure.PropertyName, failure.ErrorMessage));
+
+        return (dynamic)errors;
+    }
+}

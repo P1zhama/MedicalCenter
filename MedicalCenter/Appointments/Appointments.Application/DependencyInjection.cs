d56761fc2ac967cb0ca6856333ba13545ae61ffd
P1zhama <vlad-serdjukov@bk.ru>
2026-08-15 23:23:00 +0300

    Appointments.Application Add DI

---
+++ MedicalCenter/Appointments/Appointments.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+using System.Reflection;
+using Appointments.Application.Common.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Appointments.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehavior<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        return services;
+    }
+}

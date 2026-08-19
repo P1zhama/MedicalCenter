b9b3841483461da0ab7abe014e8109fc274b28c8
P1zhama <vlad-serdjukov@bk.ru>
2026-08-20 00:31:15 +0300

    update AppointmentsModels to the Gateway

--- MedicalCenter/Gateway/Gateway.API/Models/AppointmentsModels.cs
+++ MedicalCenter/Gateway/Gateway.API/Models/AppointmentsModels.cs
@@ -6,3 +6,28 @@ public record AvailableSlotWebResponse(
     string StartTime,
     IReadOnlyList<string> DoctorIds
 );
+
+public record CreateAppointmentWebRequest(
+    string ServiceId,
+    string DoctorId,
+    string OfficeId,
+    string Date,
+    string StartTime
+);
+
+public record CreateAppointmentByReceptionistWebRequest(
+    string PatientId,
+    string ServiceId,
+    string DoctorId,
+    string OfficeId,
+    string Date,
+    string StartTime
+);
+
+public record RescheduleAppointmentWebRequest(
+    string DoctorId,
+    string Date,
+    string StartTime
+);
+
+public record CreatedAppointmentWebResponse(string AppointmentId);

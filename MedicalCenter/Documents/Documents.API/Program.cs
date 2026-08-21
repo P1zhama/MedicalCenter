dd31d2d888c991c28e8d7262afec02157a8d2f13
P1zhama <vlad-serdjukov@bk.ru>
2026-08-21 21:16:12 +0300

    add docker to the Documents.Api

--- MedicalCenter/Documents/Documents.API/Program.cs
+++ MedicalCenter/Documents/Documents.API/Program.cs
@@ -1,12 +1,72 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
+using Common.Api.Authentication;
+using Common.Api.Middleware;
+using Documents.Api.ErrorHandling;
+using Documents.Application;
+using Documents.Infrastructure;
+using Documents.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console(new CompactJsonFormatter())
+    .WriteTo.File(new CompactJsonFormatter(), "Logs/bootstrap-log-.json", rollingInterval: RollingInterval.Day)
+    .CreateBootstrapLogger();
 
-builder.Services.AddGrpc();
+try
+{
+    Log.Information("Starting Documents Microservice...");
+    var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services));
 
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+    AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 
-app.Run();
+    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddApplication();
+    builder.Services.AddJwtAuthentication(builder.Configuration);
+
+    builder.Services.Configure<FormOptions>(options =>
+    {
+        options.MultipartBodyLengthLimit = 6L * 1024 * 1024;
+    });
+
+    builder.Services.AddControllers();
+    builder.Services.AddProblemDetails();
+    builder.Services.AddExceptionHandler<DomainExceptionHandler>();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+    app.UseExceptionHandler();
+
+    app.UseAuthentication();
+    app.UseCurrentUser();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var context = scope.ServiceProvider.GetRequiredService<DocumentsDbContext>();
+        await context.Database.MigrateAsync();
+    }
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Documents microservice terminated unexpectedly");
+
+    Environment.ExitCode = 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
